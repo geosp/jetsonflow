7ab0574af119f9adc9106f690f65c08b78b5b4be
Geovanny Fajardo <gfajardo@govspend.com>
2020-06-04 18:20:30 -0400

    clenup

--- jetson/build.sh
+++ jetson/build.sh
@@ -1,5 +1,5 @@
 docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
 cat /proc/sys/fs/binfmt_misc/qemu-aarch64
 export DOCKER_CLI_EXPERIMENTAL=enabled
-docker buildx create --use --name jetson-builder --node jetson-builder0 --driver-opt network=host
+docker buildx create --use --name jetson-builder --node jetson-builder0 --driver-opt network=host --config=buildkitd.toml
 docker buildx build . -t jetson-tensorflow:v44 --platform linux/arm64 --load
